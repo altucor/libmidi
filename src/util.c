c75f6391cc1030efcd4e36a7d7e0256f1df01793
Sergey Nadtochii <serega96916@gmail.com>
2024-10-04 02:01:57 +0300

    Add switch for WIndows bswap

--- src/util.c
+++ src/util.c
@@ -8,7 +8,11 @@
 uint16_t readu16bswap(uint8_t *data, uint32_t *iterator)
 {
     uint16_t out = *(uint16_t *)&data[*iterator];
+#ifdef _WIN32
+    out = _byteswap_ushort(out);
+#else
     out = __builtin_bswap16(out);
+#endif
     *iterator += sizeof(uint16_t);
     return out;
 }
@@ -23,7 +27,11 @@ uint32_t readu32(uint8_t *data, uint32_t *iterator)
 uint32_t readu32bswap(uint8_t *data, uint32_t *iterator)
 {
     uint32_t out = *(uint32_t *)&data[*iterator];
+#ifdef _WIN32
+    out = _byteswap_ulong(out);
+#else
     out = __builtin_bswap32(out);
+#endif
     *iterator += sizeof(uint32_t);
     return out;
 }
